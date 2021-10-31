03fbb196b7c492c890a190668679fd9f97a2cf1b
Eduardo Henrique <eduardohds.dev@outlook.com>
2021-10-31 11:51:21 -0300

    Add mask text functions

---
+++ functions/mask-text/index.js
@@ -0,0 +1,130 @@
+/**
+ * Return as 000.000.000-00
+ * @param {string} text
+ * @returns string
+ */
+function maskCpf(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+/**
+ * Return as 00.000.000/0000-00
+ * @param {string} text
+ * @returns string
+ */
+function maskCnpj(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+/**
+ * Return as (00) 0000-0000 or (00) 00000-0000
+ * @param {string} text
+ * @returns string
+ */
+function maskPhone(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(\d{4})-(\d)(\d{4})/, "$1$2-$3")
+    .replace(/(-\d{4})\d+?$/, "$1");
+}
+
+/**
+ * Return as 00000-000
+ * @param {string} text
+ * @returns string
+ */
+function maskCep(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+}
+
+/**
+ * Return as 00/00/0000
+ * @param {string} text
+ * @returns string
+ */
+function maskDate(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{4})\d+?$/, "$1");
+}
+
+/**
+ * Return as R$ 0,00
+ * @param {string} text
+ * @returns string
+ */
+function maskMoney(text = "") {
+  return text
+    .replace(/\D/g, "")
+    .replace(/^(\d{1})$/g, "0,0$1")
+    .replace(/^(\d{2})$/g, "0,$1")
+    .replace(/^(\d{1,})(\d{2})$/g, "$1,$2")
+    .replace(/^00(,\d{2})$/g, "0$1")
+    .replace(/^00(\d)(,\d{2})$/g, "$1$2")
+    .replace(/^0(\d)(,\d{2})$/g, "$1$2")
+    .replace(/^(\d{1,})(,\d{2})$/g, "R$ $1$2");
+}
+
+/**
+ * Mask text
+ * @param {string} maskType
+ * @param {Function} onChangeText
+ * @returns {Function}
+ */
+function maskText(maskType, onChangeText) {
+  return function (text) {
+    switch (maskType) {
+      case "number":
+        onChangeText(text.replace(/\D/g, ""));
+        break;
+      case "cpf":
+        onChangeText(maskCpf(text));
+        break;
+      case "cnpj":
+        onChangeText(maskCnpj(text));
+        break;
+      case "phone":
+        onChangeText(maskPhone(text));
+        break;
+      case "cep":
+        onChangeText(maskCep(text));
+        break;
+      case "data":
+        onChangeText(maskDate(text));
+        break;
+      case "money":
+        onChangeText(maskMoney(text));
+        break;
+      default:
+        break;
+    }
+  };
+}
+
+module.exports = {
+  maskCpf,
+  maskCnpj,
+  maskPhone,
+  maskCep,
+  maskMoney,
+  maskDate,
+  maskText,
+};
