2533c258f662444c1037118cc88b7645b821e81f
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-03-31 14:46:12 -0300

    Fix ts declarations

--- functions/mask-text/index.d.ts
+++ functions/mask-text/index.d.ts
@@ -1,18 +1,18 @@
-export type MaskType = "number" | "cpf" | "cnpj" | "phone" | "cep" | "money";
+export type MaskType = 'number' | 'cpf' | 'cnpj' | 'phone' | 'cep' | 'money';
 
-export function maskCpf(text: string = ""): string;
+export function maskCpf(text: string): string;
 
-export function maskCnpj(text: string = ""): string;
+export function maskCnpj(text: string): string;
 
-export function maskPhone(text: string = ""): string;
+export function maskPhone(text: string): string;
 
-export function maskCep(text: string = ""): string;
+export function maskCep(text: string): string;
 
-export function maskDate(text: string = ""): string;
+export function maskDate(text: string): string;
 
-export function maskMoney(text: string = ""): string;
+export function maskMoney(text: string): string;
 
 export function maskText(
-  maskType: MaskType,
-  onChangeText: (text?: string) => void
+	maskType: MaskType,
+	onChangeText: (text?: string) => void
 ): (text: string) => void;
