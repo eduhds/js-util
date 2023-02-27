558470ce059a9fb5aaec05549d61ee619309a7e5
Eduardo Henrique <eduardohds.dev@outlook.com>
2023-02-27 10:37:31 -0300

    fix: formatDateBR padStart

--- functions/format/index.js
+++ functions/format/index.js
@@ -4,12 +4,10 @@
  * @returns {string} date as DD/MM/YYYY
  */
 function formatDateBR(date) {
-  if (!date) throw new Error("Date is null or undefined");
-  let day = date.getDate();
-  day = day.toString().length < 2 ? `0${day}` : day;
-  let month = date.getMonth() + 1;
-  month = month.toString().length < 2 ? `0${month}` : month;
-  return `${day}/${month}/${date.getFullYear()}`;
+  if (!date) throw new Error('Date is null or undefined');
+  let [day, month, year] = [date.getDate(), date.getMonth() + 1, date.getFullYear()];
+  const padStart = val => val.toString().padStart(2, '0');
+  return `${padStart(day)}/${padStart(month)}/${year}`;
 }
 
 /**
@@ -18,11 +16,11 @@ function formatDateBR(date) {
  * @returns {string}
  */
 function formatMoneyBR(value) {
-  if (typeof value !== "string") throw new Error("Param is not a string");
+  if (typeof value !== 'string') throw new Error('Param is not a string');
   return value
     .toFixed(2)
-    .replace(".", ",")
-    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    .replace('.', ',')
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 }
 
 /**
@@ -31,8 +29,8 @@ function formatMoneyBR(value) {
  * @returns {string}
  */
 function formatPlacaOld(value) {
-  if (typeof value !== "string") throw new Error("Param is not a string");
-  return value.toUpperCase().replace(/([A-Z]{3})([0-9]{4})/, "$1-$2");
+  if (typeof value !== 'string') throw new Error('Param is not a string');
+  return value.toUpperCase().replace(/([A-Z]{3})([0-9]{4})/, '$1-$2');
 }
 
 module.exports = {
