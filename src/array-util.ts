e0abcbf093cb5996fb68481d03c7e2263e8979fb
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-12-30 14:28:54 +0000

    fix: sortByKey types

--- src/array-util.ts
+++ src/array-util.ts
@@ -1,12 +1,12 @@
+type ObjectSortItem = { [key: string]: string };
+
 /**
  * Sort array of object by key property
  */
 export function sortByKey(k: string) {
-  return function (a: unknown, b: unknown) {
-    // @ts-ignore
-    var textA = a[k];
-    // @ts-ignore
-    var textB = b[k];
+  return function (a: ObjectSortItem, b: ObjectSortItem) {
+    let textA = a[k];
+    let textB = b[k];
     if (typeof textA === 'string') textA = textA.toUpperCase().trim();
     if (typeof textB === 'string') textB = textB.toUpperCase().trim();
     return textA < textB ? -1 : textA > textB ? 1 : 0;
