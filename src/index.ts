8b0141f7706ec47c367efcd9aaaa1373a97c32db
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-12-30 15:38:59 +0000

    fix: es target

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { sortByKey } from './array-util';
+import { getUnixEpochTimestamp, convertTimestampFromUnixEpoch, formatDateBR } from './date-util';
+import { formatMoneyBR, formatPlacaOld } from './string-util';
 
-export { sortByKey };
+export {
+  sortByKey,
+  getUnixEpochTimestamp,
+  convertTimestampFromUnixEpoch,
+  formatDateBR,
+  formatMoneyBR,
+  formatPlacaOld
+};
